8e6c80dc986d208c4588bbe07cd3142398a7a452
Todd Seller <me@toddseller.com>
2015-12-13 00:08:31 -0800

    Add honeypot to help protect from spam bots

--- contact/send_form.php
+++ contact/send_form.php
@@ -1,9 +1,14 @@
 <?php
+if(isset($_REQUEST'honeypot') && $_REQUEST'honeypot' && $_REQUEST'honeypot' != '')
+{
+  exit;
+}
 if(!isset($_POST['submit']))
 {
   //This page should not be accessed directly. Need to submit the form.
   echo "error; you need to submit the form!";
 }
+
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $ip = $_SERVER['REMOTE_ADDR'];
